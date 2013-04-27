26f7c3dd0bd529fd414527581ccce222496ab8c4
Sumeet Kumar <kr.sumeet@gmail.com>
2013-04-27 14:46:53 -0700

    intital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BatteryLifeOptimizer::Application.config.secret_token = 'c565a2ebcaae2eaf8a65f091018d3e371944389ed3ad01b5a4a57e38dfad83a3576d942c067ac035c52661c5dd19007630d2599d3eb6b16a1a0896733233c230'
